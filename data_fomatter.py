737b530545b4e462df047236552729628a87121c
ChelseaXLL <xuanlinliu7@gmail.com>
2021-09-21 18:07:53 -0400

    modules

--- data_fomatter.py
+++ data_fomatter.py
@@ -1,11 +1,19 @@
 import json
-
+import os
+import logging
+logger = logging.getLogger(__name__)
 def format_data(training_data: str, test_data: str):
     '''
     Arguments:
         - training data: relative path to the training data. ('../squad/train-v2.0.json')
         - training data: relative path to the validation data. ('../squad/dev-v2.0.json')
     '''
+    logging.basicConfig(
+        format="%(asctime)s - %(levelname)s - %(name)s -   %(message)s",
+        datefmt="%m/%d/%Y %H:%M:%S",
+        level=logging.INFO
+    )
+    
     with open(training_data,'r') as f:
         training_data = json.load(f)
     
@@ -33,10 +41,15 @@ def format_data(training_data: str, test_data: str):
                     answer = a['text']
                     val_data[question] = {'context': context, 'answer':answer}
                     
+                    
                                     
-    with open('squad_train.json','w') as f:
+    with open('squad/squad_train.json','w') as f:
         json.dump(train_data,f)
         
-    with open('squad_test.json','w') as f:
-        json.dump(val_data,f) 
+    with open('squad/squad_test.json','w') as f:
+        json.dump(val_data,f)
+        
+    logger.info(f'Fomatted data has been saved into {os.getcwd()}')
+    
+    
     
